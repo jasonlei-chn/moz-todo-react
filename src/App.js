e6bb2978efc465c31ef086dc4dc5fe3f2c37d5e7
Jiayi Lei <jasonlei.chn@gmail.com>
2020-10-12 22:11:28 -0700

    React interactivity: Events and state
    - https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_interactivity_events_state
    - finished "Detour: counting tasks"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => {
+  const [tasks, setTasks] = useState(props.tasks);
+  const addTask = (name) => {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  };
+
+  const taskList = tasks.map((task) => {
     return (
       <Todo
         name={task.name}
@@ -14,16 +21,20 @@ function App(props) {
       />
     );
   });
+
+  const tasksNoun = taskList.length > 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton name="All" />
         <FilterButton name="Active" />
         <FilterButton name="Completed" />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
